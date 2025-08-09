f10a4f5c7017b0f4e492f09f9db77c945387ee11
Sanjika Perera <sanjikamanesha@gmail.com>
2025-08-09 22:20:57 +1000

    new mongo creds

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.104.105.5:5001', // live
+  baseURL: 'http://13.239.232.155:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
