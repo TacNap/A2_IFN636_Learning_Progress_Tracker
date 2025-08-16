e040b71269527b7fee1a18f21026e080892b9a0a
Sanjika Perera <sanjikamanesha@gmail.com>
2025-08-17 01:05:22 +1000

    newip3

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://16.176.169.237:5001', // live
+  baseURL: 'http://13.210.217.215:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
